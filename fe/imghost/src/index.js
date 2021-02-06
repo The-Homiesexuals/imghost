ed0e8a1e1c9802652089538e6e3b3e444874b3c7
deenhaque <fardeen2000@hotmail.com>
2021-02-05 22:39:37 -0500

    SASS is set up?

--- fe/imghost/src/index.js
+++ fe/imghost/src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+// import './index.css';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import './custom.scss';
 
 ReactDOM.render(
   <React.StrictMode>
