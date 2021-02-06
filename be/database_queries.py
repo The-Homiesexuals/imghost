b3e8d652932ce071654d6298211c50d9ff0616fe
matthewjohnbennett <mben2010@live.ca>
2021-02-06 14:43:27 -0500

    Minor adjustments

--- be/database_queries.py
+++ be/database_queries.py
@@ -1,16 +1,3 @@
-
-# List all images and their tags
-# SELECT title,tagName FROM image JOIN image_has_tags WHERE image.imageID=image_has_tags.imageID;
-
-# List all tags of an image "searchImageName"
-# SELECT tagName FROM image_has_tags WHERE image_has_tags.imageID="searchImageName";
-
-# List all imageIDs of images with tag "searchTagName"
-# SELECT imageID FROM image_has_tags WHERE image_has_tags.tagName="searchTagName";
-
-# List all image names of images with tag "searchTagName"
-# SELECT title FROM image JOIN image_has_tags WHERE image_has_tags.imageID=image.imageID AND tagName="searchTagName";
-
 import sqlite3
 
 global conn
