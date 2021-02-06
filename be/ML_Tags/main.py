79b448b2f38043698ca8882331c867d2debff331
matthewjohnbennett <mben2010@live.ca>
2021-02-06 17:02:34 -0500

    Now using Google Vision to tag images

--- be/ML_Tags/main.py
+++ be/ML_Tags/main.py
@@ -1,3 +1,17 @@
-import numpy as np
-import tensorflow as tf
+import requests
+from PIL import Image
+from google.cloud import vision
 
+client = vision.ImageAnnotatorClient()
+
+r = requests.get('https://discord.com/channels/807390623996837939/807390714718584832/807728781653180467', stream=True)
+
+image = vision.Image(content=Image.open(r.raw))
+r.close()
+
+response = client.label_detection(image=image)
+labels = response.label_annotations
+
+print('Labels:')
+for label in labels:
+    print(label.description)
