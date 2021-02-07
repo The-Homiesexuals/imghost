77880c50ac2b8ca05aec6791d79b782383182417
matthewjohnbennett <mben2010@live.ca>
2021-02-07 00:09:32 -0500

    finished generateImageTags function

--- be/ML_Tags/main.py
+++ be/ML_Tags/main.py
@@ -3,15 +3,18 @@ import io
 from PIL import Image
 from google.cloud import vision
 
-client = vision.ImageAnnotatorClient()
+client = vision.ImageAnnotatorClient.from_service_account_file('./vision_api_key.json')
 
-r = requests.get('https://imghoststoragebucket.s3.ca-central-1.amazonaws.com/download.jpg', stream=True)
+def generateImageTags(URL):
+    r = requests.get(URL, stream=True)
 
-image = vision.Image(content=r.raw.read())
+    image = vision.Image(content=r.raw.read())
 
-response = client.label_detection(image=image)
-labels = response.label_annotations
+    response = client.label_detection(image=image)
+    labels = response.label_annotations
 
-print('Labels:')
-for label in labels:
-    print(label.description)
+    return list(map(lambda x:x.description, labels))
+
+if __name__ == "__main__":
+    print(generateImageTags('https://imghoststoragebucket.s3.ca-central-1.amazonaws.com/download.jpg'))
+    # Image is Master cheif helmet wallpaper
